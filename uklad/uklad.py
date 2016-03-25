43489691131c999ec64990bed5d690eb59c18baa
Karol Ciszek <karol.ciszek15@imperial.ac.uk>
2016-03-25 23:41:31 +0000

    Move julia and shading functions to class
    This makes a lot more sense than having many different global
    variables.
    
    Todo: clean up shading functions; decide on class structure for the
    module.

--- uklad/uklad.py
+++ uklad/uklad.py
@@ -7,62 +7,59 @@ ITER_NUM = 250
 EPS = 0.01
 BAILOUT = 2
 
-xmin, xmax = (-0.7, 0.3)
-ymin, ymax = (-0.3j, 1.3j)
-c = -0.123 + 0.745j
-abs_c = abs(c)
-
-# Todo: implement these variables as arguments
-# Todo: Move to Julia set class?
-# Exponent in the Julia set
-p = 2
-# Number of 'trailing' elements when calculating Triangle Inequality Average
-m = 3
+xmin, xmax = (-1., 1.)
+ymin, ymax = (-1.j, 1.j)
 
 width = 30
 yrange = np.abs(ymax - ymin)
 xrange = xmax - xmin
+
 height = np.int(yrange * width / xrange)
 
-
-def julia(z):
-    zs = np.empty(ITER_NUM, dtype='complex64')
-    for i in range(ITER_NUM):
-        z = z ** 2 + c
-        zs[i] = z
-        if abs(z) >= BAILOUT:
-            return i, zs
-    return ITER_NUM, zs
+path = 'out.png'
 
 
-# Functions for "Smooth Iteration Count"
-def smooth_iter(z, iters):
-    return iters + 1 + np.log(np.log(BAILOUT) / np.log(abs(z))) / np.log(p)
+class Julia:
+    # Exponent in the Julia set
+    p = 2
+    BAILOUT = 2.0
 
+    def __init__(self, c, m=3):
+        self.c = c
+        self.m = m
 
-# Functions for Triangle Inequality Average method for colouring fractals
-# Pre: zs has at least two elements
-def t(zn_minus1, zn, const):
-    abs_zn_minus1 = abs(zn_minus1 ** p)
+    def julia(self, z):
+        zs = np.empty(ITER_NUM, dtype='complex64')
+        for i in range(ITER_NUM):
+            z = z ** 2 + self.c
+            zs[i] = z
+            if abs(z) >= BAILOUT:
+                return i, zs
+        return ITER_NUM, zs
 
-    mn = abs(abs_zn_minus1 - abs(const))
-    Mn = abs_zn_minus1 + abs(const)
-    return (abs(zn) - mn) / (Mn - mn)
+    # Shading functions
+    def smooth_iter(self, z, iters):
+        return iters + 1 + np.log(np.log(BAILOUT) /
+                                  np.log(abs(z))) / np.log(self.p)
 
+    def t(self, zn_minus1, zn, const):
+        abs_zn_minus1 = abs(zn_minus1 ** self.p)
 
-# to be implemented later
-def avg_sum(zs, i, numelems, const):
-    if i - numelems == 0:
-        return np.inf
-    return (sum(t(zs[n - 2], zs[n - 1], const) for n in range(numelems, i)) /
-            (i - numelems))
+        mn = abs(abs_zn_minus1 - abs(const))
+        Mn = abs_zn_minus1 + abs(const)
+        return (abs(zn) - mn) / (Mn - mn)
 
+    def avg_sum(self, zs, i, num_elems, const):
+        if i - num_elems == 0:
+            return np.inf
+        return (
+            sum(self.t(zs[n-2], zs[n-1], const) for n in range(num_elems, i)) /
+            (i - num_elems))
 
-def lin_inp(zs, d, i, num_elems, const=c):
-    last_iters_num = i if i < num_elems else num_elems
-    return (d * avg_sum(zs, i, last_iters_num, const) +
-            (1 - d) * avg_sum(zs[:-1], i, last_iters_num, const))
-
+    def lin_inp(self, zs, d, i):
+        last_iters_num = i if i < self.m else self.m
+        return (d * self.avg_sum(zs, i, last_iters_num, self.c) +
+                (1 - d) * self.avg_sum(zs[:-1], i, last_iters_num, self.c))
 
 if __name__ == '__main__':
     if len(sys.argv) == 2:
@@ -72,15 +69,16 @@ if __name__ == '__main__':
     yaxis = np.linspace(ymin, ymax, height)
 
     bitmap = np.zeros((height, width))
+    julia = Julia(-0.8 + 0.156j)
 
     for row in range(width):
         for col in range(height):
             cplx_param = xaxis[row] + yaxis[col]
-            numiters, iterated_zs = julia(cplx_param)
-            smooth_count = smooth_iter(iterated_zs[numiters - 1], numiters)
+            num_iters, iterated_zs = julia.julia(cplx_param)
+            smooth_count = julia.smooth_iter(iterated_zs[num_iters - 1], num_iters)
 
-            index = lin_inp(iterated_zs, smooth_count % 1.0,
-                            numiters, m, c)
+            index = julia.lin_inp(iterated_zs, smooth_count % 1.0,
+                                  num_iters)
             if np.isnan(index) or np.isinf(index):
                 index = 0
 
